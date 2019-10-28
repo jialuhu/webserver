de179ca67bdde4b5dd255d06ce185fdcb075ed16
胡佳露 <974520639@qq.com>
2019-10-28 17:55:49 +0800

    Create the main to test the thread+EventLoop

--- main.cpp
+++ main.cpp
@@ -8,12 +8,18 @@
 #include "Buffer.h"
 #include "TcpConnection.h"
 #include "WebServer.h"
+#include "EventLoopthread.h"
 
 int main(){
+   //std::thread::id thread;
+    //thread = std::this_thread::get_id();
+    //std::cout << "主线程ID: " << thread << "\n";
     EventLoop loop;
     InetAddr addr(8888);
     WebServer server(&loop,addr);
     server.start();
+    std::cout<<"主线程\n";
     loop.loop();
+    std::cout<<"主线程jieshu\n";
     return 0;
 }
